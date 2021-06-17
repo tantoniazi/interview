f81964d228b3d71b13489405cbacc9f14192bace
Tiago Guerra <tiago.antoniazi@gmail.com>
2021-06-17 12:29:10 -0300

    add auth and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'teste' , email: 'teste@teste.com' , password: '123123123' , password_confirmation: '123123123')
